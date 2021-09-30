8bc45802ea4baf0c0d8651c754ecf69a14de412c
Zach Caceres (zach.dev) <hello@zach.dev>
2021-09-30 17:49:43 -0600

    more exercises

---
+++ ex8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+  int i = 0;
+
+  if (argc == 1) {
+    printf("You only have one argument. Bad!\n");
+  } else if (argc > 1 && argc < 4) {
+    printf("Here are your arguments:\n");
+
+    for (i = 0; i < argc; i++) {
+      printf("%s ", argv[i]);
+    }
+    printf("\n");
+  } else {
+    printf("You have too many arguments.\n");
+  }
+
+  return 0;
+}
